e034b21c281beff03ce8d108e6206916df7d8f01
JohanAceroSalazar <johanacero2509@gmail.com>
2026-04-24 08:57:27 -0500

    Solution to the registration error

--- web/src/pages/Register/Register.jsx
+++ web/src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Register.css";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
